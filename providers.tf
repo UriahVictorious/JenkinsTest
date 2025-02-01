38f9dffd59259d0be5071747e56bd51d0609d073
jastek69 <89563369+jastek69@users.noreply.github.com>
2025-02-01 15:30:28 -0800

    first commit

---
+++ providers.tf
@@ -0,0 +1,59 @@
+# The default provider configuration; resources that begin with `aws_` will use
+# it as the default, and it can be referenced as `aws`.
+# Tokyo - HQ region
+
+
+# California - additional provider configuration for Asia Pacific region
+# reference this as `aws.california`.
+provider "aws" {
+  region = "us-west-1"
+}
+
+ 
+
+
+# Cloudwatch Requirement for us-east-1
+provider "aws" {
+  alias  = "us-east-1"
+  region = "us-east-1"
+}
+
+
+/*
+provider "aws" {
+  region = "local.region"
+}
+
+locals {
+  name   = "ex-tgw-${replace(basename(path.cwd), "_", "-")}"
+  region = "ap-northeast-1a"
+
+  tags = {
+    Example    = local.name
+    GithubRepo = "terraform-aws-eks"
+    GithubOrg  = "terraform-aws-transit-gateway"
+  }
+}
+*/
+
+
+
+
+# Providers - terraform
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.46.0" 
+    }
+  }
+  /* cloud  { 
+    
+    organization = "BHoEjM" 
+
+    workspaces { 
+      name = "zion-armageddon-bhoejm" 
+    } */ 
+  } 
+/* } */
+
